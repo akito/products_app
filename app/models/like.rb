998fff92997b2dd6e36162dde8982a5bfb0140d9
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-11 07:03:15 +0900

    Like Tableにnot null 制約を追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,8 +5,8 @@
 #  id         :bigint(8)        not null, primary key
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :bigint(8)
-#  product_id :bigint(8)
+#  user_id    :bigint(8)        not null
+#  product_id :bigint(8)        not null
 #
 
 class Like < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_215445) do
+ActiveRecord::Schema.define(version: 2018_07_10_220112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 2018_07_10_215445) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "product_id"
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
     t.index ["product_id"], name: "index_likes_on_product_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
