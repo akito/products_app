45efa5e01e4f54258e0ecbbb9d5377ae8e8b1fcc
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-10 12:06:13 +0900

    application設定を変更

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module ProductsApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.orm :active_record
+      g.test_framework :rspec, view_specs: false, routing_specs: false,
+                       helper_specs: false, request_specs: false, request_specs: false,
+                       fixture: true
+      g.template_engine :erb
+      g.assets false
+      g.helper false
+    end
   end
 end
