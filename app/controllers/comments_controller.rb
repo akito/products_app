849583794e4df7414720f491394e12c0d852872a
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-10 18:35:41 +0900

    権限エラー処理の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,13 +33,11 @@ class CommentsController < ApplicationController
     redirect_to product_path(@comment.product_id)
   end
 
-  protected
-
+  private
     def comment_params
       params.require(:comment).permit(:content, :product_id)
     end
 
-  private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
