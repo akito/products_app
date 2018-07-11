77da29b418beb2fdfb6d151dbbce158a6dba8a59
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-11 22:37:00 +0900

    Application HelperのRSpecを追加

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,14 +9,17 @@ RSpec.describe ApplicationHelper, type: :helper do
       end
     end
     context '@productが指定されてる場合' do
-      # TODO: Productの作り方がわからない
-      # before do
-      #   assign(:product.name, "hoge")
-      # end
-      # it 'ページタイトルに@product.nameが入った文字列が返されること' do
-      #   stub_const('ApplicationHelper::APP_NAME', 'Products APP')
-      #   expect(helper.page_title).to eq("Products APP | hoge")
-      # end
+      it 'ページタイトルに@product.nameが入った文字列が返されること' do
+
+        @product = Product.new(
+          name: 'product_name',
+          url: 'https://example.com',
+          desc: 'this is cool service.'
+        )
+
+        stub_const('ApplicationHelper::APP_NAME', 'Products APP')
+        expect(helper.page_title).to eq("Products APP | product_name")
+      end
     end
   end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,100 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  name                   :string
+#
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'is valid with a name, email, password' do
+    user = User.new(
+      name: 'user',
+      email: 'user@test.com',
+      password: 'usrpassword')
+    expect(user.valid?).to be_truthy
+  end
+
+  it 'is invalid without a name' do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without an email address' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'is invalid with a duplicate name' do
+    User.create(
+      name: 'user',
+      email: 'user@test.com',
+      password: 'usrpassword'
+    )
+    user = User.new(
+      name: 'user',
+      email: 'user02@test.com',
+      password: 'usrpassword'
+    )
+    user.valid?
+    expect(user.errors[:name]).to include("has already been taken")
+  end
+
+  it 'is invalid with a duplicate email address' do
+    User.create(
+      name: 'user',
+      email: 'user@test.com',
+      password: 'usrpassword'
+    )
+    user = User.new(
+      name: 'user02',
+      email: 'user@test.com',
+      password: 'usrpassword'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it 'is invalid user name with 16 or more characters' do
+    user = User.new(
+      name: "a" * 16,
+      email: 'user@test.com',
+      password: 'usrpassword')
+    user.valid?
+    expect(user.valid?).to be_falsey
+  end
+  it 'is invalid user name with 2 or less characters' do
+    user = User.new(
+      name: "a" * 2,
+      email: 'user@test.com',
+      password: 'usrpassword')
+    user.valid?
+    expect(user.valid?).to be_falsey
+  end
+
+  it 'is invalid user name with symbols that can not be used' do
+    user = User.new(
+      name: "a1$!@",
+      email: 'user@test.com',
+      password: 'usrpassword')
+    user.valid?
+    expect(user.valid?).to be_falsey
+  end
+
+end
