c72327430318421805361aa34fec663a19663673
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-12 18:18:54 +0900

    Categoryの操作権限を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
+  resources :products
+  resources :categories
   resources :comments, only: [:create, :edit, :update, :destroy]
   resources :likes, only: [:create, :destroy]
 end
