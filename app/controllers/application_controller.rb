710e132e4ee0c695d6681efdfd2d409a1268e9f3
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-12 16:32:13 +0900

    管理画面のアクセス制御を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   include ErrorHandler
-  #
-  # rescue_from SecurityError do |exception|
-  #   redirect_to root_url, notice: 'アドミン画面へのアクセス権がありません'
-  # end
 
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
-  #
-  # def authenticate_admin_user!
-  #   raise SecurityError unless current_user.try(:admin?)
-  # end
+
+    def authenticate_admin_user!
+      raise Forbidden unless current_user.try(:admin?)
+    end
 end
--- db/fixtures/01_users.rb
+++ db/fixtures/01_users.rb
@@ -1,19 +1,27 @@
 User.seed(:id) do |s|
   s.id = 1
+  s.name = "admin"
+  s.email = "admin@test.com"
+  s.password = "password"
+  s.role = "admin"
+end
+
+User.seed(:id) do |s|
+  s.id = 2
   s.name = "user01"
   s.email = "user01@test.com"
   s.password = "password"
 end
 
 User.seed(:id) do |s|
-  s.id = 2
+  s.id = 3
   s.name = "user02"
   s.email = "user02@test.com"
   s.password = "password"
 end
 
 User.seed(:id) do |s|
-  s.id = 3
+  s.id = 4
   s.name = "user03"
   s.email = "user03@test.com"
   s.password = "password"
