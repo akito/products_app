c274a31bf3727aaf7a5cebd9d5a6f7a24a4d2484
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-27 02:45:14 +0900

    Deveseのエラーメッセージの表記方法を修正した

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module ProductsApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :en
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
