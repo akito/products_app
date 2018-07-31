1cb88593588e7669a77694c67bc2f5ac10a18fb9
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-07-31 09:03:52 +0900

    Facebookで登録buttonを作成した

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.published.includes(:category).order(:id)
+    @products = Product.published.order(:id)
   end
 
   # GET /products/1
