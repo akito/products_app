cf42ba1290bf6cb5151851795413f4c32bae0d49
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-08-30 10:55:22 +0900

    [fix]Active アドミンのバグを修正

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Product do
-  permit_params :name, :url, :desc, :image, :thumbnail, :status
+  permit_params :name, :url, :desc, :image, :thumbnail, :status, :category_id, :sub_title, :ogpimage, :twitter, :advertisement
 end
